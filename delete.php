737d7ffe50ae98f5b7bd0d392fe2d4863f05e860
wcdesigns <wcdesigns@users.noreply.github.com>
2015-07-31 14:43:55 -0400

    Delete.php
    Delete.php is the file that is used, when the Delete button is pressed, to delete records from the employees table.

---
+++ delete.php
@@ -0,0 +1,53 @@
+<?php
+    include_once 'includes/database.php';
+    $id = 0;
+     
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( !empty($_POST)) {
+        // Keep track of the posted values
+        $id = $_POST['id'];
+         
+        // Delete the data
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM employees  WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+        header("Location: index.php");
+         
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Delete - <?php echo $data['name'];?></title>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Delete <?php echo $data['name'];?></h3>
+                        </div>
+                     
+                    <form class="form-horizontal" action="delete.php" method="post">
+                      <input type="hidden" name="id" value="<?php echo $id;?>"/>
+                      <p class="alert alert-error">Are you sure to delete ?</p>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-danger">Yes</button>
+                          <a class="btn" href="index.php">No</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
